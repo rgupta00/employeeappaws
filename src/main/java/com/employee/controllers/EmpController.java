7f0b3e9d4764bd4fce29255644fac108081d9073
busycoderacademy <105028843+busycoderacademy@users.noreply.github.com>
2025-02-28 14:02:43 +0530

    Update EmpController.java

--- src/main/java/com/employee/controllers/EmpController.java
+++ src/main/java/com/employee/controllers/EmpController.java
@@ -4,7 +4,7 @@ import com.employee.services.InstanceInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//now code is added 
 @RestController
 public class EmpController {
 
